a1507a761f27a1be402ead516b52e61b94793034
achigua <andreshamt765@gmail.com>
2025-02-24 19:59:52 -0500

    Actividad_1 - configuracion

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="",
+    version="0.0.1",
+    author="Bibiana Roman",
+    author_email="bibiana.roman@est.iudigital.edu.co",
+    description="",
+    py_modules=["Actividad_1"],
+    install_requires=[
+        "pandas",
+        "matplotlib"
+    ]
+
+
+)
