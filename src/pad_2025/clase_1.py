d14795c64e8c68bca33c91bb3ee13f995873a3ff
achigua <andreshamt765@gmail.com>
2025-02-24 21:54:12 -0500

    clase 1 - programacion orientada a objetos

---
+++ src/pad_2025/clase_1.py
@@ -0,0 +1,12 @@
+class Personas():
+    def __init__(self):
+        self.nombre = ""
+        self.edad = 0
+        self.estatura = 0
+        self.peso = 0
+
+persona = Personas()
+persona.nombre = "Andres"   
+persona.edad = 38
+persona.estatura = 168     
+print("Nombre: ",persona.nombre, " Edad: ",persona.edad, " Estatura: ",persona.estatura)
